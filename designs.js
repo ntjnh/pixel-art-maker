0cf8e005c6709c6a91a632ef06afae877be5995d
Nate Jonah <nate@natejonah.com>
2017-11-23 12:31:45 +0000

    Save color and grid dimension values to variables and create loop for grid height

--- designs.js
+++ designs.js
@@ -1,23 +1,34 @@
 $(document).ready(function() {
-
-// Select color input
-const colorPicker = $("#colorPicker")
-;
-// Select size input
-const inputHeight = $("#input_height");
-const inputWidth = $("#input_width");
-
-// When size is submitted by the user, call makeGrid()
-$("#sizePicker").submit(function makeGrid() {
-
+  const colorPicker = $("#colorPicker");
   const pixels = $("#pixel_canvas");
-  // 
-
-  // Loop for creatintg grid based on input dimensions
+  const color = colorPicker.val();
   
+  function makeGrid(height, width) {
   
+    // Loop for creating grid based on input dimensions
+    for (let i = 0; i < height; i++) {
+      $("<tr>").appendTo(pixels);
+      
+      // for (let j = 0; j < width; j++) {
+      //   $("<td>").ao
+      // }
+    }
+    
+  }
   
-});
+  // When size is submitted by the user, call makeGrid()
+  $("#sizePicker").submit(function(e) {
+    e.preventDefault();
+    
+    const inputHeight = $("#input_height").val(); // number of tds
+    const inputWidth = $("#input_width").val();  // number of trs
+    
+    
+    
+    
+    
+    makeGrid(inputHeight, inputWidth);
+  });
 
 
 });
