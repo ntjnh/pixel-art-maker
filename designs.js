a0172b294ee984fe989b4767e43756add13439bf
Nate Jonah <natehenj@gmail.com>
2017-11-23 11:05:10 +0000

    Connect jQuery and assign variables for DOM elements

--- designs.js
+++ designs.js
@@ -1,10 +1,23 @@
+$(document).ready(function() {
+
 // Select color input
+const colorPicker = $("#colorPicker")
+;
 // Select size input
+const inputHeight = $("#input_height");
+const inputWidth = $("#input_width");
 
 // When size is submitted by the user, call makeGrid()
+$("#sizePicker").submit(function makeGrid() {
 
-function makeGrid() {
+  const pixels = $("#pixel_canvas");
+  // 
 
-// Your code goes here!
+  // Loop for creatintg grid based on input dimensions
+  
+  
+  
+});
 
-}
+
+});
