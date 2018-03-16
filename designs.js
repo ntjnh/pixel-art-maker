6b1e0380b46e57f94a6995b2d4919a90a39bf278
Nate Jonah <nate@natejonah.com>
2018-03-16 00:00:10 +0000

    Replace jQuery with plain JavaScript

--- designs.js
+++ designs.js
@@ -1,47 +1,38 @@
-$(document).ready(function() {
-  const colorPicker = $("#colorPicker");
-  const pixels = $("#pixel_canvas");
+const colorPicker = document.getElementById("colorPicker");
+const pixels = document.getElementById("pixel_canvas");
+
+function makeGrid(height, width) {
+  // Get rid of the old grid first
+  pixels.innerHTML = "";
   
-  function makeGrid(height, width) {
-    // Get rid of the old grid first
-    pixels.empty();
+  for (let i = 0; i < height; i++) {
+    // Create rows based on height
+    const row = document.createElement("tr");
+    pixels.appendChild(row);
     
-    for (let i = 0; i < height; i++) {
-      // Create rows based on height
-      const row = $("<tr>").appendTo(pixels);
-      
-      for (let j = 0; j < width; j++) {
-        // Create columns based on width and add them to the rows
-        $("<td>").appendTo(row);
-      }
+    for (let j = 0; j < width; j++) {
+      // Create columns based on width and add them to the rows
+      const cell = document.createElement("td");
+      row.appendChild(cell);
     }
-    
   }
+}
+
+// When size is submitted by the user, call makeGrid()
+const sizePicker = document.getElementById("sizePicker");
+sizePicker.addEventListener("submit", function(e) {
+  e.preventDefault();
   
-  // When size is submitted by the user, call makeGrid()
-  $("#sizePicker").submit(function(e) {
-    e.preventDefault();
-    
-    const inputHeight = $("#input_height").val();
-    const inputWidth = $("#input_width").val();
+  const inputHeight = document.getElementById("input_height").value;
+  const inputWidth = document.getElementById("input_width").value;
 
-    makeGrid(inputHeight, inputWidth);
-  });
+  makeGrid(inputHeight, inputWidth);
+});
 
-  // Event listener for grid squares
-  $(pixels).mousedown(function(e) {
-    const color = colorPicker.val();
-    const clickedSquare = e.target;
-    const clicks = $(clickedSquare).data("clicks");
-    
-    if (clicks) {
-      // On 2nd click, revert back to original background color
-      $(clickedSquare).css("background-color", "transparent");
-    } else {
-      // Change background color of clicked square
-      $(clickedSquare).css("background-color", color);
-    }
-    $(clickedSquare).data("clicks", !clicks);
-  });
-
+// Event listener for grid squares
+pixels.addEventListener("mousedown", function(e) {
+  const color = colorPicker.value;
+  const clickedSquare = e.target;
+  // Change background color of clicked square
+  clickedSquare.style.backgroundColor = color;
 });
